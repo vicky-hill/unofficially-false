878e7253eb27a6e7096bf09ddb7cfe82d04044e9
vicky-hill <vicky.ungemach95@gmail.com>
2025-11-28 17:09:15 -0800

    move router into useeffect

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@
 
 import Protect from '@/components/layout/Protect'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 
 export default function Home() {
   const router = useRouter();
+
+  useEffect(() => {
+    router.push('/drinks')
+  }, [])
 
   router.push('/drinks')
 
